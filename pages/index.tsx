30724f8467d2ee09c405a499c2731f7d0e8e6acd
Sergey Mazaev <s.mazaev@bars.group>
2022-10-04 15:34:07 +0600

    feat: #12 add description at page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,19 @@ import { Calculation } from '../components/calculation/Calculation';
 
 const Home: NextPage = () => {
 	return (
-		<div className={styles.container}>
-			<Calculation />
-		</div>
+		<>
+			<h3 className={styles.header}>FIRE калькулятор</h3>
+			<div className={styles.container}>
+				<div className={styles.mainContainer}>
+					<div>
+						Калькулятор для расчета возраста выхода на пенсию по американской
+						системе FIRE. По этой системе необходимо накопить денег на 25 лет
+						жизни по текущему уровню трат.
+					</div>
+					<Calculation />
+				</div>
+			</div>
+		</>
 	);
 };
 
